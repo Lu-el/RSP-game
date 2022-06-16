12a78cfe7c84d9dadbc2054f7a13836dd460ab54
Lu-el <lubov.elunina@mail.ru>
2022-06-16 18:02:49 +0400

    correcting

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ window.gameRSP = (() => {
   const FIGURE_ENG = ['rock', 'scissors', 'paper'];
   const FIGURE_RUS = ['камень', 'ножницы', 'бумага'];
 
+  const WORDS_RU = ['Компьютер', 'Ты', 'Результат игры', 'выиграл', 'Ничья', 'Вы точно хотите выйти?'];
+  const WORDS_ENG = ['PS', 'You', 'Game result', 'won', 'Draw', 'Are you sure?'];
+
   const getRandomInclusive = (min, max) =>
     Math.round(Math.random() * (max - min) + min);
 
@@ -22,130 +25,49 @@ window.gameRSP = (() => {
     const lang = language === 'EN' || language === 'ENG' ?
       FIGURE_ENG : FIGURE_RUS;
 
+    const words = language === 'EN' || language === 'ENG' ?
+      WORDS_ENG : WORDS_RU;
+
     const message = lang.map(item => item.concat('? ')).join('');
 
-    const conditions = (a, b) => {
-      if (a[0] === b && (language === 'EN' || language === 'ENG')) {
-        return alert(
-          `PS: ${a}
-        You: ${a}
-        A draw`
-        )
-      };
-      if (a[0] === b) {
-        return alert(
-          `Компьютер: ${a}
-        Вы: ${a}
-        Ничья`
-        )
+    const condition = (a, b) => {
+      let winner;
+      if (a === b) {
+        winner = words[4];
+      } else {
+        winner = getWinner(a, b) === lang[a] ? words[0] : words[1];
+        if (winner === words[0]) {
+          result.computer += 1;
+        } else {
+          result.player += 1;
+        }
+        winner += ' ' + words[3];
       }
-      const key = a[0] + ' ' + b;
-      switch (key) {
-        case 'к н':
-          alert(
-            `Компьютер: камень
-          Вы: ножницы
-          Компьютер выиграл`);
-          result.computer += 1;
-          break;
-        case 'к б':
-          alert(
-            `Компьютер: камень
-            Вы: бумага
-            Вы выиграли`);
-          result.player += 1;
-          break;
-        case 'н б':
-          alert(
-            `Компьютер: ножницы
-            Вы: бумага
-            Компьютер выиграл`);
-          result.computer += 1;
-          break;
-        case 'н к':
-          alert(
-            `Компьютер: ножницы
-            Вы: камень
-            Вы выиграли`);
-          result.player += 1;
-          break;
-        case 'б к':
-          alert(
-            `Компьютер: бумага
-            Вы: камень
-            Компьютер выиграл`);
-          result.computer += 1;
-          break;
-        case 'б н':
-          alert(
-            `Компьютер: бумага
-            Вы: ножницы
-            Вы выиграли`);
-          result.player += 1;
-          break;
+      return alert(
+        `${words[0]}: ${lang[a]}
+        ${words[1]}: ${lang[b]}
+        ${winner}`
+      )
+    }
 
-        case 'r s':
-          alert(
-            `PS: rock
-            You: scissors
-            PS won`);
-          result.computer += 1;
-          break;
-        case 'r p':
-          alert(
-            `PS: rock
-              You: paper
-              You won`);
-          result.player += 1;
-          break;
-        case 's p':
-          alert(
-            `PS: scissors
-              You: paper
-              PS won`);
-          result.computer += 1;
-          break;
-        case 's r':
-          alert(
-            `PS: scissors
-              You: rock
-              You won`);
-          result.player += 1;
-          break;
-        case 'p r':
-          alert(
-            `PS: paper
-              You: rock
-              PS won`);
-          result.computer += 1;
-          break;
-        case 'p s':
-          alert(
-            `PS: paper
-              You: scissors
-              You won`);
-          result.player += 1;
-          break;
-        default:
-          break;
+    const getWinner = (f, s) => {
+      const min = f > s ? s : f;
+      const max = f < s ? s : f;
+      if (min === 0 && max === 2) {
+        return lang[max];
+      } else {
+        return lang[min];
       }
     }
 
     return function start() {
       const playerQuest = prompt(message);
       if (playerQuest === null) {
-        if ((language === 'EN' || language === 'ENG') && confirm('Are you sure?')) {
+        if (confirm(`${words[5]}`)) {
           return alert(
-            `Game result:
-        PS: ${result.computer}
-        YOU: ${result.player}`
-          )
-        }
-        if (confirm('Вы точно хотите выйти?')) {
-          return alert(
-            `Результат игры:
-        Компьютер: ${result.computer}
-        Вы: ${result.player}`
+            `${words[2]}:
+            ${words[0]}: ${result.computer}
+            ${words[1]}: ${result.player}`
           )
         } else {
           return start();
@@ -153,7 +75,11 @@ window.gameRSP = (() => {
       }
       const computerVariant = getFigure(language);
       const playerVariant = playerQuest[0] || '';
-      conditions(computerVariant, playerVariant.toLowerCase());
+
+      const indexComp = lang.indexOf(computerVariant);
+      const indexPlayer = lang.indexOf(lang.find(element => element[0] === playerVariant));
+
+      condition(indexComp, indexPlayer);
       start();
     };
   };
--- script.js
+++ script.js
@@ -3,4 +3,4 @@
 const startGameRU = window.gameRSP.game();
 const startGameEN = window.gameRSP.game('EN');
 
-// startGameEN();
+startGameRU();
